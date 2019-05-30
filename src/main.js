e22398260aa0137480aa88e7154a739e8b89ee60
Masaki Koyanagi <mascii@gmail.com>
2019-05-30 18:51:54 +0900

    data-props can be empty string

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const elementsShouldBeMounted = document.querySelectorAll('div[data-props]');
 
   for (const el of elementsShouldBeMounted) {
-    const props = JSON.parse(el.getAttribute('data-props'));
+    const props = JSON.parse(el.getAttribute('data-props') || '{}');
     const getVueInstance = collection[el.id];
     if (getVueInstance) {
       getVueInstance(props).$mount('#' + el.id);
